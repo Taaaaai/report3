7fc52c6e99e0db131c914bc851102a5d897fe976
Taisei Arakaki <e175746@ie.u-ryukyu.ac.jp>
2017-11-13 12:50:06 +0900

    final2 change

--- src/main/jp/ac/uryukyu/ie/e175746/Main.java
+++ src/main/jp/ac/uryukyu/ie/e175746/Main.java
@@ -2,7 +2,7 @@ package jp.ac.uryukyu.ie.e175746;
 
 public class Main {
     public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
+        Hero hero = new Hero("橘", 10, 5);
         Enemy enemy = new Enemy("スライム", 6, 3);
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
